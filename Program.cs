ae3275d3ae1a7e5bcbc0ac9bcb99ee138b5b4b0a
PRIYANKA <PRIYANKA@LAPTOP-GO8TVDUH>
2023-12-08 04:37:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 5; j >= i; j--)
+                {
+                    // Star pattern with no extra space at start
+                    if (i == j)
+                    {
+                        for (int k = 1; k <= i; k++)
+                        {
+                            Console.Write("*");
+                        }
+                    }
+                    Console.Write(" ");
+                }
+
+                // Star pattern with extra space at start
+                //for (int k = 1; k <= i; k++)
+                //{
+                //    Console.Write("*");
+                //}
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
